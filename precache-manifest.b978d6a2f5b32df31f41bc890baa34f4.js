4df7ae467c17ebb869fc6335ac680527a45d4cc1
Andrei Anufriev <63143166+Paladin-From-335@users.noreply.github.com>
2020-09-24 15:28:26 +0300

    Add files via upload

---
+++ precache-manifest.b978d6a2f5b32df31f41bc890baa34f4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3762757bf8efc7943a7c0f9566848c1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb6003f742bf87b6ecc",
+    "url": "/static/css/main.587e45cb.chunk.css"
+  },
+  {
+    "revision": "5e7f4ef68557b9a19014",
+    "url": "/static/js/2.fce4ca42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fce4ca42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb6003f742bf87b6ecc",
+    "url": "/static/js/main.b20e58c6.chunk.js"
+  },
+  {
+    "revision": "446525eda61f8f4c8da6",
+    "url": "/static/js/runtime-main.7210a1e0.js"
+  },
+  {
+    "revision": "f5d726c43998961f680f05977ac02a38",
+    "url": "/static/media/ava1.f5d726c4.jpg"
+  },
+  {
+    "revision": "95c4c698dfe8ab6f6751af6a7a6ce4df",
+    "url": "/static/media/facebook.95c4c698.png"
+  },
+  {
+    "revision": "f92ffa5dab38c86bd49d0ba82d53bb5c",
+    "url": "/static/media/inst.f92ffa5d.svg"
+  },
+  {
+    "revision": "41e59d2ab76379e43f25939175d31b5f",
+    "url": "/static/media/phone.41e59d2a.png"
+  },
+  {
+    "revision": "f8c4c3fa873b881c438c8a925660ddfc",
+    "url": "/static/media/photo1.f8c4c3fa.jpg"
+  },
+  {
+    "revision": "e332fd95c76e1809fcee3d200ebd44fb",
+    "url": "/static/media/photo10.e332fd95.jpg"
+  },
+  {
+    "revision": "40d5a0ec5076c52be8a8791f5b33d0e2",
+    "url": "/static/media/photo11.40d5a0ec.jpg"
+  },
+  {
+    "revision": "f7689a362d43a645105b0a2aa5a54da3",
+    "url": "/static/media/photo2.f7689a36.jpg"
+  },
+  {
+    "revision": "de2b528334c487256992b9553ddd5443",
+    "url": "/static/media/photo3.de2b5283.jpg"
+  },
+  {
+    "revision": "3cf2cf377dad6fdd74ed8fa54d789cf8",
+    "url": "/static/media/photo4.3cf2cf37.jpg"
+  },
+  {
+    "revision": "09f84af4c0856ad766766ce1b9612359",
+    "url": "/static/media/photo5.09f84af4.jpg"
+  },
+  {
+    "revision": "f3465e586ec55a96d3237725f1c6f8b3",
+    "url": "/static/media/photo6.f3465e58.jpg"
+  },
+  {
+    "revision": "9d2f37ca5563e81bd00ce33aa7781e0e",
+    "url": "/static/media/photo7.9d2f37ca.jpg"
+  },
+  {
+    "revision": "6d702a4e85ba3022778985c137722cff",
+    "url": "/static/media/photo8.6d702a4e.jpg"
+  },
+  {
+    "revision": "7bf9a05d9b7064c1b09abb92b81657ea",
+    "url": "/static/media/photo9.7bf9a05d.jpg"
+  },
+  {
+    "revision": "1265cef59ea2bf7f19c4a3df1633b6d2",
+    "url": "/static/media/telega.1265cef5.png"
+  }
+]);
